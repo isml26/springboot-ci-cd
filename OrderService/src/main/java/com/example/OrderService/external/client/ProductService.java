8a31c4872ee1839d3c7054b6f68ca8caa617370f
isml26 <iguven1999@hotmail.com>
2023-08-18 16:11:23 +0300

    circuit breaker & rate limiter

--- OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
@@ -1,13 +1,20 @@
 package com.example.OrderService.external.client;
 
+import com.example.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{productId}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("productId") long productId, @RequestParam long quantity);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product Service is not available","UNAVAILABLE",500);
+    }
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+### SPRINGBOOT PROJECT
+
+- Implementing RESTful Web Services
+- Implementing SpringData JPA
+- Implementing Service Registry
+- Implementing Exception Handling
+- Implementing Feign Client & RestTemplate
+- Implementing API Gateway
+- Implementing Distributed Log Tracing(Zipkin)
+- Implementing Circuit Breaker in API
+- Implementing Rate Limiter in API Gateway using Resilience 4j and Redis
