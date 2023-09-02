fd336978e6cbda0a56de049bf88c8b184afd5447
isml26 <iguven1999@hotmail.com>
2023-09-02 23:51:41 +0300

    test update

--- OrderService/src/test/java/com/example/OrderService/controller/OrderControllerTest.java
+++ OrderService/src/test/java/com/example/OrderService/controller/OrderControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.OrderService.controller;
 
 import com.example.OrderService.OrderServiceConfig;
+import com.example.OrderService.entity.Order;
+import com.example.OrderService.model.OrderRequest;
+import com.example.OrderService.model.PaymentMode;
 import com.example.OrderService.repository.OrderRepository;
 import com.example.OrderService.service.OrderService;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -9,25 +12,36 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest("{server.port=0}")
+
+@SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {OrderServiceConfig.class})
@@ -42,7 +56,12 @@ class OrderControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer
+            = WireMockExtension.newInstance()
             .options(WireMockConfiguration
                     .wireMockConfig()
                     .port(8080))
@@ -64,6 +83,7 @@ class OrderControllerTest {
     }
 
     private void reduceQuantity() {
+        circuitBreakerRegistry.circuitBreaker("external").reset();
         wireMockServer.stubFor(put(urlMatching("/products/reduceQuantity/.*"))
                 .willReturn(aResponse().withStatus(HttpStatus.OK.value())
                         .withHeader(MediaType.APPLICATION_JSON_VALUE))
@@ -71,6 +91,7 @@ class OrderControllerTest {
     }
 
     private void getPaymentDetails() throws IOException {
+        circuitBreakerRegistry.circuitBreaker("external").reset();
         wireMockServer.stubFor(WireMock.get(urlMatching("/payment/.*"))
                 .willReturn(aResponse().withStatus(HttpStatus.OK.value())
                         .withHeader(MediaType.APPLICATION_JSON_VALUE)
@@ -82,6 +103,7 @@ class OrderControllerTest {
     }
 
     private void doPayment() {
+        circuitBreakerRegistry.circuitBreaker("external").reset();
         wireMockServer.stubFor(WireMock.post(("/payment"))
                 .willReturn(aResponse().withStatus(HttpStatus.OK.value())
                         .withHeader(MediaType.APPLICATION_JSON_VALUE)));
@@ -100,16 +122,42 @@ class OrderControllerTest {
                         ))));
     }
 
+    private OrderRequest getMockOrderRequest() {
+        return OrderRequest.builder()
+                .productId(1)
+                .paymentMode(PaymentMode.CASH)
+                .quantity(10)
+                .totalAmount(200)
+                .build();
+    }
+
 
     @Test
-    public void test_WhenPlaceOrder_DoPayment_Success(){
+    public void test_WhenPlaceOrder_DoPayment_Success() throws Exception {
         // Place order
-
 
         // Get order by OrderId from DB
 
         // Check output
 
-    }
+        OrderRequest orderRequest = getMockOrderRequest();
 
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.post("/orders/placeOrder")
+                .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("Customer")))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(orderRequest))
+        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        String orderId = mvcResult.getResponse().getContentAsString();
+
+        Optional<Order> order = orderRepository.findById(Long.valueOf(orderId));
+        Assertions.assertTrue(order.isPresent());
+
+        Order getOrder = order.get();
+
+        Assertions.assertEquals(Long.parseLong(orderId),getOrder.getOrderId());
+        Assertions.assertEquals(orderRequest.getTotalAmount(),getOrder.getOrderAmount());
+        Assertions.assertEquals(orderRequest.getQuantity(),getOrder.getQuantity());
+
+    }
 }
