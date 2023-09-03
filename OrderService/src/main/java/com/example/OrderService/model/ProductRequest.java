6ff1fd044524c73cf0a463f48118af582c40d2de
isml26 <iguven1999@hotmail.com>
2023-09-03 14:02:04 +0300

    order controller test

---
+++ OrderService/src/main/java/com/example/OrderService/model/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.example.OrderService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductRequest {
+    private String productName;
+    private long price;
+    private long quantity;
+}
--- OrderService/src/test/java/com/example/OrderService/controller/OrderControllerTest.java
+++ OrderService/src/test/java/com/example/OrderService/controller/OrderControllerTest.java
@@ -3,9 +3,12 @@ package com.example.OrderService.controller;
 import com.example.OrderService.OrderServiceConfig;
 import com.example.OrderService.entity.Order;
 import com.example.OrderService.model.OrderRequest;
+import com.example.OrderService.model.OrderResponse;
 import com.example.OrderService.model.PaymentMode;
+import com.example.OrderService.model.ProductRequest;
 import com.example.OrderService.repository.OrderRepository;
 import com.example.OrderService.service.OrderService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -38,7 +41,10 @@ import java.nio.charset.Charset;
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static java.nio.charset.Charset.defaultCharset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.util.StreamUtils.copyToString;
 
 
 @SpringBootTest({"server.port=0"})
@@ -76,6 +82,7 @@ class OrderControllerTest {
 
     @BeforeEach
     void setup() throws IOException {
+//        addProduct();
         getProductDetailsResponse();
         doPayment();
         getPaymentDetails();
@@ -85,28 +92,50 @@ class OrderControllerTest {
     private void reduceQuantity() {
         circuitBreakerRegistry.circuitBreaker("external").reset();
         wireMockServer.stubFor(put(urlMatching("/products/reduceQuantity/.*"))
-                .willReturn(aResponse().withStatus(HttpStatus.OK.value())
-                        .withHeader(MediaType.APPLICATION_JSON_VALUE))
-        );
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
     }
+
+
+//    private void addProduct() throws JsonProcessingException {
+//        ProductRequest productRequest = new ProductRequest();
+//        productRequest.setProductName("SamsungS20");
+//        productRequest.setPrice(1500);
+//        productRequest.setQuantity(200);
+//
+//        circuitBreakerRegistry.circuitBreaker("external").reset();
+//        wireMockServer.stubFor(post(urlMatching("/products"))
+//                .withRequestBody(equalToJson(objectMapper.writeValueAsString(productRequest))) // Check the JSON equality
+//                .willReturn(aResponse()
+//                        .withStatus(HttpStatus.OK.value())
+//                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+//        );
+//    }
+
+
 
     private void getPaymentDetails() throws IOException {
         circuitBreakerRegistry.circuitBreaker("external").reset();
-        wireMockServer.stubFor(WireMock.get(urlMatching("/payment/.*"))
-                .willReturn(aResponse().withStatus(HttpStatus.OK.value())
-                        .withHeader(MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(StreamUtils.copyToString(
-                                OrderController.class.getClassLoader()
-                                        .getResourceAsStream("mock/GetPayment.json"),
-                                Charset.defaultCharset())))
-        );
+        wireMockServer.stubFor(get(urlMatching("/payment/.*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(
+                                copyToString(
+                                        OrderControllerTest.class
+                                                .getClassLoader()
+                                                .getResourceAsStream("mock/GetPayment.json"),
+                                        defaultCharset()
+                                )
+                        )));
     }
 
     private void doPayment() {
-        circuitBreakerRegistry.circuitBreaker("external").reset();
-        wireMockServer.stubFor(WireMock.post(("/payment"))
-                .willReturn(aResponse().withStatus(HttpStatus.OK.value())
-                        .withHeader(MediaType.APPLICATION_JSON_VALUE)));
+        wireMockServer.stubFor(post(urlEqualTo("/payment"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
     }
 
     private void getProductDetailsResponse() throws IOException {
@@ -115,7 +144,7 @@ class OrderControllerTest {
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(StreamUtils.copyToString(
+                        .withBody(copyToString(
                                 OrderController.class.getClassLoader()
                                         .getResourceAsStream("mock/GetProduct.json"),
                                 Charset.defaultCharset()
@@ -143,7 +172,7 @@ class OrderControllerTest {
         OrderRequest orderRequest = getMockOrderRequest();
 
         MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.post("/orders/placeOrder")
-                .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("Customer")))
+                .with(jwt().authorities(new SimpleGrantedAuthority("Customer")))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(objectMapper.writeValueAsString(orderRequest))
         ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
@@ -155,9 +184,85 @@ class OrderControllerTest {
 
         Order getOrder = order.get();
 
-        Assertions.assertEquals(Long.parseLong(orderId),getOrder.getOrderId());
-        Assertions.assertEquals(orderRequest.getTotalAmount(),getOrder.getOrderAmount());
-        Assertions.assertEquals(orderRequest.getQuantity(),getOrder.getQuantity());
+        assertEquals(Long.parseLong(orderId),getOrder.getOrderId());
+        assertEquals(orderRequest.getTotalAmount(),getOrder.getOrderAmount());
+        assertEquals(orderRequest.getQuantity(),getOrder.getQuantity());
 
     }
+
+    @Test
+    public void test_WhenPlaceOrderWithWringAccess_thenThrow403() throws Exception {
+
+        OrderRequest orderRequest = getMockOrderRequest();
+
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.post("/orders/placeOrder")
+                .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(orderRequest))
+        ).andExpect(MockMvcResultMatchers.status().isForbidden()).andReturn();
+    }
+
+    // To get correct result of that test we should run all test from the beginning of the class
+    // Run class OrderControllerTest
+    @Test
+    public void test_WhenGetOrder_Success() throws Exception {
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.get("/orders/1")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+        Order order = orderRepository.findById(1l).get();
+        String expectedResponse = getOrderResponse(order);
+
+        assertEquals(expectedResponse,actualResponse);
+    }
+
+    @Test
+    public void test_When_GetOrder_Order_Not_Found() throws Exception {
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.get("/orders/2")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+
+    }
+
+
+    private String getOrderResponse(Order order) throws IOException {
+        OrderResponse.PaymentDetails paymentDetails
+                = objectMapper.readValue(
+                copyToString(
+                        OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("mock/GetPayment.json"
+                                ),
+                        defaultCharset()
+                ), OrderResponse.PaymentDetails.class
+        );
+        paymentDetails.setPaymentStatus("SUCCESS");
+
+        OrderResponse.ProductDetails productDetails
+                = objectMapper.readValue(
+                copyToString(
+                        OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("mock/GetProduct.json"),
+                        defaultCharset()
+                ), OrderResponse.ProductDetails.class
+        );
+
+        OrderResponse orderResponse
+                = OrderResponse.builder()
+                .paymentDetails(paymentDetails)
+                .productDetails(productDetails)
+                .orderStatus(order.getOrderStatus())
+                .orderDate(order.getOrderDate())
+                .amount(order.getOrderAmount())
+                .orderId(order.getOrderId())
+                .build();
+        return objectMapper.writeValueAsString(orderResponse);
+    }
+
 }
